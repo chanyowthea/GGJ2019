328260b11ec433d6f802f6be849a04cbb79729bb
IIzzaya <linxuan_0709@icloud.com>
2019-01-26 19:57:23 +0800

    优化

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,39 +11,42 @@ public class CameraController : MonoBehaviour {
 	private PlayerController player;
 	public Transform cameraFollowTarget;
 
+	bool isMoving;
+	float moveSpeed = 70f;
+
 	public void UpdateRoom(Room room) {
 		this.room.rotateTarget.gameObject.SetActive(false);
-		// var numb = room.rotateTarget.transform.childCount;
-		// for (int i = 0; i < numb; i++) {
-		// 	var item = room.rotateTarget.transform.GetChild(i);
-		// 	var mesh = item.GetComponent<MeshRenderer>();
-		// 	if (mesh != null) mesh.enabled = true;
-
-		// 	var meshes = item.GetComponentsInChildren<MeshRenderer>();
-		// 	foreach (var subItem in meshes) {
-		// 		if (subItem != null) subItem.enabled = true;
-		// 	}
-		// }
 
 		this.room = room;
 
 		this.room.rotateTarget.gameObject.SetActive(true);
-		// numb = room.rotateTarget.transform.childCount;
-		// for (int i = 0; i < numb; i++) {
-		// 	var item = room.rotateTarget.transform.GetChild(i);
-		// 	var mesh = item.GetComponent<MeshRenderer>();
-		// 	if (mesh != null) mesh.enabled = true;
-
-		// 	var meshes = item.GetComponentsInChildren<MeshRenderer>();
-		// 	foreach (var subItem in meshes) {
-		// 		if (subItem != null) subItem.enabled = true;
-		// 	}
-		// }
 
 		transform.position = room.cameraRect.transform.position;
-		vc.LookAt = room.transform;
-		cameraFollowTarget.position = room.transform.position;
+		// vc.LookAt = room.transform;
+		isMoving = true;
+		counter = 0;
+		moveVector = room.transform.position - cameraFollowTarget.position;
+		// cameraFollowTarget.position = room.transform.position;
 		room.HideWall();
+	}
+
+	Vector3 moveVector;
+	float counter = 0;
+	private void Update() {
+		if (isMoving) {
+
+			var len = moveVector.magnitude;
+
+			if (counter < len) {
+				cameraFollowTarget.Translate(moveVector.normalized * moveSpeed * Time.deltaTime);
+				counter += moveSpeed * Time.deltaTime;
+			} else {
+				cameraFollowTarget.position = room.transform.position;
+				isMoving = false;
+			}
+
+		}
+
 	}
 
 	private void Awake() {
--- Assets/Scripts/ConstValue.cs
+++ Assets/Scripts/ConstValue.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public static class ConstValue
 {
     public const int _RoomRotateSpeed = 1; // angle per frame.  
-    public const int _PlayerMoveSpeed = 4; // uint per second.  
+    public const int _CameraMoveSpeed = 4; // uint per second.  
 }
