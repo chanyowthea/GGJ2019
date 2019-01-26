0c5a13db2f7b56adcc550944de41adf1b5739108
chanyowthea <454982165@qq.com>
2019-01-26 15:38:51 +0800

    1

---
+++ Assets/Scripts/Room/RoomCameraManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomCameraManager : MonoBehaviour
+{
+    [SerializeField] Camera _Camera; 
+    [SerializeField] Transform _Corner; 
+    [SerializeField] Transform _Center; 
+
+    // Use this for initialization
+    void Start()
+    {
+        var dir = _Center.position - _Corner.position; 
+        _Camera.transform.position = dir.normalized * 14f + Vector3.up * 6; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Room/RoomCreator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomCreator : MonoBehaviour
+{
+    [SerializeField] Transform _WallLeft;
+    [SerializeField] Transform _WallRight;
+    [SerializeField] Transform _WallFront;
+    [SerializeField] Transform _WallBack;
+    [SerializeField] Transform _WallFloor;
+    [SerializeField] Transform _WallCeiling;
+
+    [SerializeField] Transform _FrontRightCorner;
+    [SerializeField] Transform _BackRightCorner;
+    [SerializeField] Transform _FrontLeftCorner;
+    [SerializeField] Transform _BackLeftCorner;
+    [SerializeField] Transform _RoomCenter;
+
+    private void Start()
+    {
+        SetData(20);
+    }
+
+    public void SetData(int roomSize = 10)
+    {
+        float halfSize = (roomSize - 1) / 2f; 
+        _WallLeft.transform.localScale = new Vector3(roomSize, 1, roomSize);
+        _WallLeft.transform.localEulerAngles = new Vector3(0, 0, 90);
+        _WallLeft.transform.localPosition = new Vector3(-halfSize, halfSize, 0); 
+
+        _WallRight.transform.localScale = new Vector3(roomSize, 1, roomSize);
+        _WallRight.transform.localEulerAngles = new Vector3(0, 0, 90);
+        _WallRight.transform.localPosition = new Vector3(halfSize, halfSize, 0);
+
+        _WallFront.transform.localScale = new Vector3(roomSize, 1, roomSize);
+        _WallFront.transform.localEulerAngles = new Vector3(0, 90, 90);
+        _WallFront.transform.localPosition = new Vector3(0, halfSize, halfSize);
+
+        _WallBack.transform.localScale = new Vector3(roomSize, 1, roomSize);
+        _WallBack.transform.localEulerAngles = new Vector3(0, 90, 90);
+        _WallBack.transform.localPosition = new Vector3(0, halfSize, -halfSize);
+
+        _WallFloor.transform.localScale = new Vector3(roomSize, 1, roomSize);
+        _WallFloor.transform.localEulerAngles = new Vector3(0, 0, 0);
+        _WallFloor.transform.localPosition = new Vector3(0, 0, 0);
+
+        _WallCeiling.transform.localScale = new Vector3(roomSize, 1, roomSize);
+        _WallCeiling.transform.localEulerAngles = new Vector3(0, 0, 0);
+        _WallCeiling.transform.localPosition = new Vector3(0, 0, halfSize * 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
