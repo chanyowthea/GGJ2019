8b8621996065538809a5d42cd7216b8b029912c8
chanyowthea <454982165@qq.com>
2019-01-26 11:23:07 +0800

    Create RoomRotator.cs

---
+++ Assets/Scripts/Room/RoomRotator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ERotateAxis
+{
+    X,
+    Y,
+    Z,
+}
+
+public class RoomRotator : MonoBehaviour
+{
+    public void Rotate(int angle, ERotateAxis axis)
+    {
+        var a = this.transform.localEulerAngles;
+        if (axis == ERotateAxis.X)
+        {
+            this.transform.Rotate(angle, 0, 0);
+        }
+        else if (axis == ERotateAxis.Y)
+        {
+            this.transform.Rotate(0, angle, 0);
+        }
+        else if (axis == ERotateAxis.Z)
+        {
+            this.transform.Rotate(0, 0, angle);
+        }
+    }
+}
